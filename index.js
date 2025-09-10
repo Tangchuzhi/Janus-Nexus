c6ae45b8cc2362879238eb8e6d19a3b4c5688cfd
chuzhitang <382944938@qq.com>
2025-09-10 13:50:13 +0800

    index.js

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 (function() {
     'use strict';
-
-    const extensionName = 'janus-treasure-chest';
-    const extensionFolderPath = `scripts/extensions/${extensionName}`;
     
-    let settings = {};
-
-    // 扩展初始化
-    function init() {
-        console.log('Janus Treasure Chest 扩展已加载');
-        loadSettings();
-        createUI();
-    }
-
-    // 加载设置
-    function loadSettings() {
-        settings = extension_settings[extensionName] || {};
-    }
-
-    // 保存设置
-    function saveSettings() {
-        extension_settings[extensionName] = settings;
-        saveSettingsDebounced();
-    }
-
-    // 创建UI界面
-    function createUI() {
-        // 在这里添加你的UI代码
+    console.log('🎉 Janus Treasure Chest 扩展已加载！');
+    
+    // 简单测试：添加一个按钮到扩展设置页面
+    jQuery(() => {
         const html = `
-            <div id="janus-treasure-chest">
-                <h3>Janus宝藏箱</h3>
-                <div class="janus-controls">
-                    <!-- 添加你的控件 -->
-                </div>
+            <div id="janus-treasure-chest" class="extension-settings">
+                <h3>🎁 Janus宝藏箱</h3>
+                <p>扩展已成功加载！</p>
+                <button onclick="alert('Janus扩展工作正常！')">测试按钮</button>
             </div>
+        `;
+        
+        $('#extensions_settings2').append(html);
+    });
+})();
         `;
         
         $('#extensions_settings').append(html);
