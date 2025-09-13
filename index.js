fab4cdd62e73949cf89c4ebfdc600d372362d7fd
唐初稚 <382944938@qq.com>
2025-09-13 14:36:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -456,7 +456,7 @@ jQuery(() => {
                     <div class="game-icon">${game.icon || '🎮'}</div>
                     <div class="game-details">
                         <div class="game-name">${game.name}</div>
-                        <div class="game-meta" style="font-size: 0.8em; opacity: 0.8; line-height: 1.1;">
+                        <div class="game-meta" style="font-size: 0.8em; opacity: 0.8; line-height: 1.1; display: block;">
                             <span>游戏来源: ${game.source === 'file' ? '[文件]' : '[URL]'}${game.type || ''}</span><br>
                             <span>导入时间: ${new Date(game.importedAt).toLocaleDateString()}</span>
                         </div>
